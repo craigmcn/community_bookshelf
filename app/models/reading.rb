cf3a96afc7f27f0e0d0a56ea020ef893e626a8eb
Craig McN <craig.mcnaughton@animikii.com>
2026-06-05 20:17:51 -0700

    Add scope for retrieving records with soft-deletion

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,6 +3,7 @@ class Reading < ApplicationRecord
   belongs_to :book
 
   default_scope { where(deleted_at: nil) }
+  scope :with_deleted, -> { unscoped }
 
   enum :status, {want_to_read: 0, reading: 1, finished: 2}
   enum :rating, {one: 1, two: 2, three: 3, four: 4, five: 5}
